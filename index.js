4bebedc27841119961616c64e8eb5a6745e2f69c
CoderJay06 <jaycru06@yahoo.com>
2021-01-17 03:39:23 -0500

    commit tree 53efa5b34bb95656ea677c1d47be3e0078e22c4e

---
+++ index.js
@@ -0,0 +1,21 @@
+// objects
+// Scooter constructor
+// 1) should create a new scooter with three properties
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+}
+// Driver constructor
+// 2) should create a new driver with three properties
+function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+// PickupLocation constructor
+// 3) should create a new PickupLocation with two properties
+function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+}
